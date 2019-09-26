aa7df18b7e00fc8093b2be04e28ded71bf2de7e6
ftinh <47810779+ftinh@users.noreply.github.com>
2019-09-26 12:42:27 +0800

    Add files via upload

---
+++ add_version2.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php 
+include('config.php');
+
+if(isset($_POST['submit']))
+{
+	$id=$_POST['id'];
+	$nama_pelajar=$_POST['nama_pelajar'];
+	$no_kp=$_POST['no_kp'];
+
+	$query1=mysqli_query($con,"INSERT INTO pelajar values('$id','$nama_pelajar','$no_kp')");
+
+	if($query1)
+{
+	header("location:senarai_pelajar.php");
+}
+}
+?>
+
+<center>
+	<fieldset style="width: 500px;">
+		<h4>TAMBAH REKOD PELAJAR</h4>
+
+		<form method="post" action="add_version2.php">
+			ID PELAJAR: <input type="text" name="id"><br>
+			NAMA PELAJAR:<input type="text" name="nama_pelajar"><br>
+			NO KP: <input type="text" name="no_kp"><br>
+			<br><input type="submit" name="submit"><br>
+
+		</form>
+	</fieldset>
+</center>
+</body>
+</html>
